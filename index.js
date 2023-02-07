496faa6f633b6374ec45fcbf1ec0f4c9a7078572
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-02-07 14:13:51 +0600

    add 3rd answer

--- index.js
+++ index.js
@@ -6,10 +6,34 @@ function mindGame(inputNumber) {
 
 // 2.evenOdd Functon
 function evenOdd(inputString) {
-    var length = inputString.length;
+    let length = inputString.length;
     if (length % 2 === 0) {
         return "even";
     } else {
         return "odd";
     }
 }
+
+// 3.isLGSeven Function
+function isLGSeven(num) {
+    if (num < 7) {
+        return 7 - num;
+    } else {
+        return num * 2;
+    }
+}
+
+// 4.findingBadData Function
+function findingBadData(numbers) {
+    let count = 0;
+    for (const num of numbers) {
+        if (num < 0) {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+
+
