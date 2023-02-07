18a1af3b89a5e798b46fc0313b9f72a30bdb3032
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-02-07 13:48:07 +0600

    add 2nd answe

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+// 1.mindGame Function
 function mindGame(inputNumber) {
-    return ((inputNumber * 3 + 10) / 2 - 5);
+    let number = ((inputNumber * 3 + 10) / 2 - 5)
+    return number;
 }
 
-let result = mindGame(8);
-console.log(result);
+// 2.evenOdd Functon
+function evenOdd(inputString) {
+    var length = inputString.length;
+    if (length % 2 === 0) {
+        return "even";
+    } else {
+        return "odd";
+    }
+}
